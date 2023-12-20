c3f21cac1aea1b2655f0c7efc7a51dd063ce7493
Antoine Karam <antoinekaram1414@gmail.com>
2023-12-20 18:32:36 +0200

    Player can now see buffs applied on him

--- Assets/Script/Particles/csDestroyEffect.cs
+++ Assets/Script/Particles/csDestroyEffect.cs
@@ -5,9 +5,6 @@ public class csDestroyEffect : MonoBehaviour {
 
 	void Update ()
     {
-	    if(Input.GetKeyDown(KeyCode.X) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.C))
-        {
-            Destroy(gameObject);
-        }
-	}
+        Destroy(gameObject, 10.0f);
+    }
 }
